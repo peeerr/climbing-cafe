2b7546579feeb2be80d7f29b6a1eab20d49ded01
JunHyeongChoi <ajfeh22@gmail.com>
2024-01-08 19:07:30 +0900

    프로젝트 세팅 (#1)

---
+++ src/main/java/com/peeerr/climbing/ClimbingApplication.java
@@ -0,0 +1,13 @@
+package com.peeerr.climbing;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ClimbingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ClimbingApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/peeerr/climbing/ClimbingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.peeerr.climbing;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ClimbingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
