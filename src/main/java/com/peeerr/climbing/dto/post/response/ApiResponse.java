15e865bf5ba17c29fe15380e55899b89f38a3648
JunHyeongChoi <ajfeh22@gmail.com>
2024-01-09 18:13:44 +0900

    게시물 단건조회, 작성, 수정, 삭제 API 구현 (#5)

---
+++ src/main/java/com/peeerr/climbing/dto/post/response/ApiResponse.java
@@ -0,0 +1,19 @@
+package com.peeerr.climbing.dto.post.response;
+
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@AllArgsConstructor(access = AccessLevel.PRIVATE)
+@Getter
+public class ApiResponse<T> {
+
+    private String status;
+    private String message;
+    private T data;
+
+    public static <T> ApiResponse<T> of(String status, String message, T data) {
+        return new ApiResponse<>(status, message, data);
+    }
+
+}
