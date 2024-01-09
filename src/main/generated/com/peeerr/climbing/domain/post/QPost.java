f6ca6d785968527cd512e2db58a34d66244b9456
JunHyeongChoi <ajfeh22@gmail.com>
2024-01-09 18:10:57 +0900

    Q클래스 생성 (#5)

---
+++ src/main/generated/com/peeerr/climbing/domain/post/QPost.java
@@ -0,0 +1,49 @@
+package com.peeerr.climbing.domain.post;
+
+import static com.querydsl.core.types.PathMetadataFactory.*;
+
+import com.querydsl.core.types.dsl.*;
+
+import com.querydsl.core.types.PathMetadata;
+import javax.annotation.processing.Generated;
+import com.querydsl.core.types.Path;
+
+
+/**
+ * QPost is a Querydsl query type for Post
+ */
+@Generated("com.querydsl.codegen.DefaultEntitySerializer")
+public class QPost extends EntityPathBase<Post> {
+
+    private static final long serialVersionUID = -1263733799L;
+
+    public static final QPost post = new QPost("post");
+
+    public final com.peeerr.climbing.domain.QBaseEntity _super = new com.peeerr.climbing.domain.QBaseEntity(this);
+
+    public final StringPath content = createString("content");
+
+    //inherited
+    public final DateTimePath<java.time.LocalDateTime> createDate = _super.createDate;
+
+    public final NumberPath<Long> id = createNumber("id", Long.class);
+
+    //inherited
+    public final DateTimePath<java.time.LocalDateTime> modifyDate = _super.modifyDate;
+
+    public final StringPath title = createString("title");
+
+    public QPost(String variable) {
+        super(Post.class, forVariable(variable));
+    }
+
+    public QPost(Path<? extends Post> path) {
+        super(path.getType(), path.getMetadata());
+    }
+
+    public QPost(PathMetadata metadata) {
+        super(Post.class, metadata);
+    }
+
+}
+
