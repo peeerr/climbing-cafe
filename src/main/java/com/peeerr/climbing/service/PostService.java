bd069fb9a4fb647458616af678bf2053994f4f41
JunHyeongChoi <ajfeh22@gmail.com>
2024-01-13 22:36:47 +0900

    조회시 DTO로 반환하게끔 수정 (#7)
    실수로 엔티티를 반환해 주던 걸 DTO로 응답하게 수정

--- src/main/java/com/peeerr/climbing/service/PostService.java
+++ src/main/java/com/peeerr/climbing/service/PostService.java
@@ -1,25 +1,37 @@
 package com.peeerr.climbing.service;
 
+import com.peeerr.climbing.domain.category.Category;
+import com.peeerr.climbing.domain.category.CategoryRepository;
 import com.peeerr.climbing.domain.post.Post;
 import com.peeerr.climbing.domain.post.PostRepository;
+import com.peeerr.climbing.dto.post.request.PostCreateRequest;
 import com.peeerr.climbing.dto.post.request.PostEditRequest;
 import com.peeerr.climbing.dto.post.response.PostResponse;
 import com.peeerr.climbing.exception.ex.EntityNotFoundException;
 import lombok.RequiredArgsConstructor;
 import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageImpl;
 import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+import java.util.stream.Collectors;
 
 @RequiredArgsConstructor
 @Service
 public class PostService {
 
     private final PostRepository postRepository;
+    private final CategoryRepository categoryRepository;
 
     @Transactional(readOnly = true)
-    public Page<Post> getPosts(Pageable pageable) {
-        return postRepository.findAll(pageable);
+    public Page<PostResponse> getPosts(Pageable pageable) {
+        List<PostResponse> posts = postRepository.findAll(pageable).stream()
+                .map(PostResponse::from)
+                .collect(Collectors.toList());
+
+        return new PageImpl<>(posts, pageable, posts.size());
     }
 
     @Transactional(readOnly = true)
@@ -32,7 +44,13 @@ public class PostService {
     }
 
     @Transactional
-    public void addPost(Post post) {
+    public void addPost(PostCreateRequest postCreateRequest) {
+        Post post = Post.builder()
+                .title(postCreateRequest.getTitle())
+                .content(postCreateRequest.getContent())
+                .category(getCategory(postCreateRequest.getCategoryId()))
+                .build();
+
         postRepository.save(post);
     }
 
@@ -43,11 +61,17 @@ public class PostService {
 
         post.changeTitle(postEditRequest.getTitle());
         post.changeContent(postEditRequest.getContent());
+        post.changeCategory(getCategory(postEditRequest.getCategoryId()));
     }
 
     @Transactional
     public void removePost(Long postId) {
         postRepository.deleteById(postId);
+    }
+
+    private Category getCategory(Long categoryId) {
+        return categoryRepository.findById(categoryId)
+                .orElseThrow(() -> new EntityNotFoundException("카테고리를 찾을 수 없습니다."));
     }
 
 }
