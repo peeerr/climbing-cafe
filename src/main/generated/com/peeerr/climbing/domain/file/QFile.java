a556e6fb06e6874a8255127811d497948932450a
JunHyeongChoi <ajfeh22@gmail.com>
2024-01-21 16:50:39 +0900

    파일 Q클래스 생성 (#9)

---
+++ src/main/generated/com/peeerr/climbing/domain/file/QFile.java
@@ -0,0 +1,65 @@
+package com.peeerr.climbing.domain.file;
+
+import static com.querydsl.core.types.PathMetadataFactory.*;
+
+import com.querydsl.core.types.dsl.*;
+
+import com.querydsl.core.types.PathMetadata;
+import javax.annotation.processing.Generated;
+import com.querydsl.core.types.Path;
+import com.querydsl.core.types.dsl.PathInits;
+
+
+/**
+ * QFile is a Querydsl query type for File
+ */
+@Generated("com.querydsl.codegen.DefaultEntitySerializer")
+public class QFile extends EntityPathBase<File> {
+
+    private static final long serialVersionUID = -288318119L;
+
+    private static final PathInits INITS = PathInits.DIRECT2;
+
+    public static final QFile file = new QFile("file");
+
+    public final com.peeerr.climbing.domain.QBaseEntity _super = new com.peeerr.climbing.domain.QBaseEntity(this);
+
+    //inherited
+    public final DateTimePath<java.time.LocalDateTime> createDate = _super.createDate;
+
+    public final StringPath filename = createString("filename");
+
+    public final StringPath filePath = createString("filePath");
+
+    public final NumberPath<Long> id = createNumber("id", Long.class);
+
+    //inherited
+    public final DateTimePath<java.time.LocalDateTime> modifyDate = _super.modifyDate;
+
+    public final StringPath originalFilename = createString("originalFilename");
+
+    public final com.peeerr.climbing.domain.post.QPost post;
+
+    public QFile(String variable) {
+        this(File.class, forVariable(variable), INITS);
+    }
+
+    public QFile(Path<? extends File> path) {
+        this(path.getType(), path.getMetadata(), PathInits.getFor(path.getMetadata(), INITS));
+    }
+
+    public QFile(PathMetadata metadata) {
+        this(metadata, PathInits.getFor(metadata, INITS));
+    }
+
+    public QFile(PathMetadata metadata, PathInits inits) {
+        this(File.class, metadata, inits);
+    }
+
+    public QFile(Class<? extends File> type, PathMetadata metadata, PathInits inits) {
+        super(type, metadata, inits);
+        this.post = inits.isInitialized("post") ? new com.peeerr.climbing.domain.post.QPost(forProperty("post"), inits.get("post")) : null;
+    }
+
+}
+
