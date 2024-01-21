bd43ed9b97c1a6e93631f21099278c3cbb4b1af3
JunHyeongChoi <ajfeh22@gmail.com>
2024-01-21 21:48:03 +0900

    정적 팩토리 메서드 추가 (#9)

--- src/main/java/com/peeerr/climbing/dto/file/request/FileUploadRequest.java
+++ src/main/java/com/peeerr/climbing/dto/file/request/FileUploadRequest.java
@@ -1,12 +1,15 @@
 package com.peeerr.climbing.dto.file.request;
 
 import jakarta.validation.constraints.NotNull;
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.Setter;
 import org.springframework.web.multipart.MultipartFile;
 
 import java.util.List;
 
+@AllArgsConstructor(access = AccessLevel.PRIVATE)
 @Getter
 @Setter
 public class FileUploadRequest {
@@ -14,5 +17,9 @@ public class FileUploadRequest {
     @NotNull
     private Long postId;
     private List<MultipartFile> files;
+
+    public static FileUploadRequest of(Long postId, List<MultipartFile> files) {
+        return new FileUploadRequest(postId, files);
+    }
 
 }
