c91846b54671d69f3281addba8382e875bd332e1
JunHyeongChoi <ajfeh22@gmail.com>
2024-01-21 16:52:48 +0900

    File 도메인 생성 (#9)

---
+++ src/main/java/com/peeerr/climbing/domain/file/File.java
@@ -0,0 +1,42 @@
+package com.peeerr.climbing.domain.file;
+
+import com.peeerr.climbing.domain.BaseEntity;
+import com.peeerr.climbing.domain.post.Post;
+import jakarta.persistence.*;
+import lombok.AccessLevel;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@Entity
+public class File extends BaseEntity {
+
+    @Column(name = "file_id")
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Id
+    private Long id;
+
+    @JoinColumn(name = "post_id", nullable = false)
+    @ManyToOne(fetch = FetchType.LAZY)
+    private Post post;
+
+    @Column(nullable = false)
+    private String originalFilename;
+
+    @Column(nullable = false)
+    private String filename;
+
+    @Column(nullable = false)
+    private String filePath;
+
+    @Builder
+    public File(Post post, String originalFilename, String filename, String filePath) {
+        this.post = post;
+        this.originalFilename = originalFilename;
+        this.filename = filename;
+        this.filePath = filePath;
+    }
+
+}
---
+++ src/main/java/com/peeerr/climbing/domain/file/FileRepository.java
@@ -0,0 +1,6 @@
+package com.peeerr.climbing.domain.file;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface FileRepository extends JpaRepository<File, Long> {
+}
