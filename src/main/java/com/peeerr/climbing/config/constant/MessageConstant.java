0556f3488a4a013bacf5443fedf838961d226802
JunHyeongChoi <ajfeh22@gmail.com>
2024-01-23 22:04:10 +0900

    메시지 분류 주석 수정 (#10)

--- src/main/java/com/peeerr/climbing/config/constant/MessageConstant.java
+++ src/main/java/com/peeerr/climbing/config/constant/MessageConstant.java
@@ -7,7 +7,8 @@ public class MessageConstant {
     public static final String NO_FILE_SELECTED = "파일을 선택하지 않았습니다. 파일을 선택해주세요.";
 
     /* StorageManagement */
-
+    public static final String FILE_STORE_FAILED = "파일 저장에 실패했습니다.";
+    public static final String DIRECTORY_CREATE_FAILED = "파일을 저장할 디렉토리 생성에 실패했습니다.";
 
     /* Post */
     public static final String POST_NOT_FOUND = "존재하지 않는 게시물입니다.";
@@ -18,7 +19,5 @@ public class MessageConstant {
     /* File */
     public static final String FILE_NOT_FOUND = "존재하지 않는 파일입니다.";
     public static final String FILE_SIZE_EXCEEDED = "전체 파일 크기를 10MB 이하로 첨부해 주세요.";
-    public static final String FILE_STORE_FAILED = "파일 저장에 실패했습니다.";
-    public static final String DIRECTORY_CREATE_FAILED = "파일을 저장할 디렉토리 생성에 실패했습니다.";
 
 }
