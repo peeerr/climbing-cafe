4707c5a62054ae5b1bb44c15411cbb37ae963390
JunHyeongChoi <ajfeh22@gmail.com>
2024-01-24 22:29:52 +0900

    유효성 검사 AOP 처리 (#10)

---
+++ src/main/java/com/peeerr/climbing/aop/ValidationCheckAspect.java
@@ -0,0 +1,43 @@
+package com.peeerr.climbing.aop;
+
+import com.peeerr.climbing.config.constant.MessageConstant;
+import com.peeerr.climbing.exception.ex.ValidationException;
+import org.aspectj.lang.ProceedingJoinPoint;
+import org.aspectj.lang.annotation.Around;
+import org.aspectj.lang.annotation.Aspect;
+import org.springframework.stereotype.Component;
+import org.springframework.validation.BindingResult;
+import org.springframework.validation.FieldError;
+
+import java.util.HashMap;
+import java.util.Map;
+
+@Component
+@Aspect
+public class ValidationCheckAspect {
+
+    @Around("execution(* com.peeerr.climbing.controller.*Controller.*(..))")
+    public Object advice(ProceedingJoinPoint proceedingJoinPoint) throws Throwable {
+        Object[] args = proceedingJoinPoint.getArgs();
+
+        for (Object arg: args) {
+            if (arg instanceof BindingResult) {
+                BindingResult bindingResult = (BindingResult) arg;
+
+                if (bindingResult.hasErrors()) {
+                    Map<String, String> errorMap = new HashMap<>();
+
+                    for (FieldError error: bindingResult.getFieldErrors()) {
+                        errorMap.put(error.getField(), error.getDefaultMessage());
+                    }
+
+                    throw new ValidationException(MessageConstant.VALIDATION_ERROR, errorMap);
+                }
+
+            }
+        }
+
+        return proceedingJoinPoint.proceed();
+    }
+
+}
