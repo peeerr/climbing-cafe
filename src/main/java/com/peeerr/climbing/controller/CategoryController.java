594de9debbfe82acc6005f66c8ec99c6886e7fc7
JunHyeongChoi <ajfeh22@gmail.com>
2024-01-24 22:28:54 +0900

    유효성 검사 중복 제거 및 공통응답 처리 (#10)

--- src/main/java/com/peeerr/climbing/controller/CategoryController.java
+++ src/main/java/com/peeerr/climbing/controller/CategoryController.java
@@ -1,22 +1,18 @@
 package com.peeerr.climbing.controller;
 
-import com.peeerr.climbing.config.constant.MessageConstant;
 import com.peeerr.climbing.dto.category.request.CategoryCreateRequest;
 import com.peeerr.climbing.dto.category.request.CategoryEditRequest;
 import com.peeerr.climbing.dto.category.response.CategoryResponse;
-import com.peeerr.climbing.exception.ex.ValidationException;
+import com.peeerr.climbing.dto.common.ApiResponse;
 import com.peeerr.climbing.service.CategoryService;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.BindingResult;
-import org.springframework.validation.FieldError;
 import org.springframework.web.bind.annotation.*;
 
-import java.util.HashMap;
 import java.util.List;
-import java.util.Map;
 
 @RequiredArgsConstructor
 @RequestMapping("/api/categories")
@@ -26,61 +22,46 @@ public class CategoryController {
     private final CategoryService categoryService;
 
     @GetMapping
-    public ResponseEntity<List<CategoryResponse>> categoryList() {
+    public ResponseEntity<ApiResponse> categoryList() {
         List<CategoryResponse> categories = categoryService.getCategories();
 
-        return ResponseEntity.ok().body(categories);
+        return ResponseEntity.ok()
+                .body(ApiResponse.success(categories));
     }
 
     @GetMapping("/{categoryId}")
-    public ResponseEntity<CategoryResponse> categoryDetail(@PathVariable Long categoryId) {
+    public ResponseEntity<ApiResponse> categoryDetail(@PathVariable Long categoryId) {
         CategoryResponse category = categoryService.getCategory(categoryId);
 
-        return ResponseEntity.ok().body(category);
+        return ResponseEntity.ok()
+                .body(ApiResponse.success(category));
     }
 
     @PostMapping
-    public ResponseEntity<Void> categoryAdd(@RequestBody @Valid CategoryCreateRequest categoryCreateRequest,
+    public ResponseEntity<ApiResponse> categoryAdd(@RequestBody @Valid CategoryCreateRequest categoryCreateRequest,
                                          BindingResult bindingResult) {
-        if (bindingResult.hasErrors()) {
-            Map<String, String> errorMap = new HashMap<>();
+        CategoryResponse addedCategory = categoryService.addCategory(categoryCreateRequest);
 
-            for (FieldError error: bindingResult.getFieldErrors()) {
-                errorMap.put(error.getField(), error.getDefaultMessage());
-            }
-
-            throw new ValidationException(MessageConstant.VALIDATION_ERROR, errorMap);
-        }
-
-        categoryService.addCategory(categoryCreateRequest);
-
-        return ResponseEntity.status(HttpStatus.CREATED).build();
+        return ResponseEntity.status(HttpStatus.CREATED)
+                .body(ApiResponse.success(addedCategory));
     }
 
     @PutMapping("/{categoryId}")
-    public ResponseEntity<Void> categoryEdit(@PathVariable Long categoryId,
+    public ResponseEntity<ApiResponse> categoryEdit(@PathVariable Long categoryId,
                                           @RequestBody @Valid CategoryEditRequest categoryEditRequest,
                                           BindingResult bindingResult) {
-        if (bindingResult.hasErrors()) {
-            Map<String, String> errorMap = new HashMap<>();
+        CategoryResponse editedCategory = categoryService.editCategory(categoryId, categoryEditRequest);
 
-            for (FieldError error: bindingResult.getFieldErrors()) {
-                errorMap.put(error.getField(), error.getDefaultMessage());
-            }
-
-            throw new ValidationException(MessageConstant.VALIDATION_ERROR, errorMap);
-        }
-
-        categoryService.editCategory(categoryId, categoryEditRequest);
-
-        return ResponseEntity.ok().build();
+        return ResponseEntity.ok()
+                .body(ApiResponse.success(editedCategory));
     }
     
     @DeleteMapping("/{categoryId}")
-    public ResponseEntity<Void> categoryRemove(@PathVariable Long categoryId) {
+    public ResponseEntity<ApiResponse> categoryRemove(@PathVariable Long categoryId) {
         categoryService.removeCategory(categoryId);
         
-        return ResponseEntity.ok().build();
+        return ResponseEntity.ok()
+                .body(ApiResponse.success());
     }
 
 }
--- src/main/java/com/peeerr/climbing/controller/PostController.java
+++ src/main/java/com/peeerr/climbing/controller/PostController.java
@@ -1,10 +1,9 @@
 package com.peeerr.climbing.controller;
 
-import com.peeerr.climbing.config.constant.MessageConstant;
+import com.peeerr.climbing.dto.common.ApiResponse;
 import com.peeerr.climbing.dto.post.request.PostCreateRequest;
 import com.peeerr.climbing.dto.post.request.PostEditRequest;
 import com.peeerr.climbing.dto.post.response.PostResponse;
-import com.peeerr.climbing.exception.ex.ValidationException;
 import com.peeerr.climbing.service.PostService;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
@@ -15,11 +14,7 @@ import org.springframework.data.web.PageableDefault;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.BindingResult;
-import org.springframework.validation.FieldError;
 import org.springframework.web.bind.annotation.*;
-
-import java.util.HashMap;
-import java.util.Map;
 
 @RequiredArgsConstructor
 @RequestMapping("/api/posts")
@@ -29,61 +24,46 @@ public class PostController {
     private final PostService postService;
 
     @GetMapping
-    public ResponseEntity<Page<PostResponse>> postList(@PageableDefault(size = 10, sort = "id", direction = Sort.Direction.DESC) Pageable pageable) {
+    public ResponseEntity<ApiResponse> postList(@PageableDefault(size = 10, sort = "id", direction = Sort.Direction.DESC) Pageable pageable) {
         Page<PostResponse> posts = postService.getPosts(pageable);
 
-        return ResponseEntity.ok().body(posts);
+        return ResponseEntity.ok()
+                .body(ApiResponse.success(posts));
     }
 
     @GetMapping("/{postId}")
-    public ResponseEntity<PostResponse> postDetail(@PathVariable Long postId) {
-        PostResponse response = postService.getPost(postId);
+    public ResponseEntity<ApiResponse> postDetail(@PathVariable Long postId) {
+        PostResponse post = postService.getPost(postId);
 
-        return ResponseEntity.ok().body(response);
+        return ResponseEntity.ok()
+                .body(ApiResponse.success(post));
     }
 
     @PostMapping
-    public ResponseEntity<Void> postAdd(@RequestBody @Valid PostCreateRequest postCreateRequest,
+    public ResponseEntity<ApiResponse> postAdd(@RequestBody @Valid PostCreateRequest postCreateRequest,
                                   BindingResult bindingResult) {
-        if (bindingResult.hasErrors()) {
-            Map<String, String> errorMap = new HashMap<>();
+        PostResponse addedPost = postService.addPost(postCreateRequest);
 
-            for (FieldError error: bindingResult.getFieldErrors()) {
-                errorMap.put(error.getField(), error.getDefaultMessage());
-            }
-
-            throw new ValidationException(MessageConstant.VALIDATION_ERROR, errorMap);
-        }
-
-        postService.addPost(postCreateRequest);
-
-        return ResponseEntity.status(HttpStatus.CREATED).build();
+        return ResponseEntity.status(HttpStatus.CREATED)
+                .body(ApiResponse.success(addedPost));
     }
 
     @PutMapping("/{postId}")
-    public ResponseEntity<Void> postEdit(@PathVariable Long postId,
+    public ResponseEntity<ApiResponse> postEdit(@PathVariable Long postId,
                                       @RequestBody @Valid PostEditRequest postEditRequest,
                                       BindingResult bindingResult) {
-        if (bindingResult.hasErrors()) {
-            Map<String, String> errorMap = new HashMap<>();
+        PostResponse editedPost = postService.editPost(postId, postEditRequest);
 
-            for (FieldError error: bindingResult.getFieldErrors()) {
-                errorMap.put(error.getField(), error.getDefaultMessage());
-            }
-
-            throw new ValidationException(MessageConstant.VALIDATION_ERROR, errorMap);
-        }
-
-        postService.editPost(postId, postEditRequest);
-
-        return ResponseEntity.ok().build();
+        return ResponseEntity.ok()
+                .body(ApiResponse.success(editedPost));
     }
 
     @DeleteMapping("/{postId}")
-    public ResponseEntity<Void> postRemove(@PathVariable Long postId) {
+    public ResponseEntity<ApiResponse> postRemove(@PathVariable Long postId) {
         postService.removePost(postId);
 
-        return ResponseEntity.ok().build();
+        return ResponseEntity.ok()
+                .body(ApiResponse.success());
     }
 
 }
