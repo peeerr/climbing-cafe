78788c8ea5d5a857edf9d143381ec757bbc44055
JunHyeongChoi <ajfeh22@gmail.com>
2024-02-03 16:54:27 +0900

    회원 클래스명 변경 반영 (#13)

--- src/main/java/com/peeerr/climbing/config/auth/CustomUserDetailsService.java
+++ src/main/java/com/peeerr/climbing/config/auth/CustomUserDetailsService.java
@@ -1,8 +1,8 @@
 package com.peeerr.climbing.config.auth;
 
 import com.peeerr.climbing.exception.constant.ErrorMessage;
-import com.peeerr.climbing.domain.user.User;
-import com.peeerr.climbing.domain.user.UserRepository;
+import com.peeerr.climbing.domain.user.Member;
+import com.peeerr.climbing.domain.user.MemberRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
@@ -13,14 +13,14 @@ import org.springframework.stereotype.Service;
 @Service
 public class CustomUserDetailsService implements UserDetailsService {
 
-    private final UserRepository userRepository;
+    private final MemberRepository memberRepository;
 
     @Override
     public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
-        User user = userRepository.findUserByUsername(username).orElseThrow(
+        Member member = memberRepository.findUserByUsername(username).orElseThrow(
                 () -> new UsernameNotFoundException(ErrorMessage.USER_NOT_FOUND));
 
-        return new CustomUserDetails(user);
+        return new CustomUserDetails(member);
     }
 
 }
--- src/main/java/com/peeerr/climbing/domain/user/MemberRepository.java
+++ src/main/java/com/peeerr/climbing/domain/user/MemberRepository.java
@@ -4,8 +4,8 @@ import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.Optional;
 
-public interface UserRepository extends JpaRepository<User, Long> {
+public interface MemberRepository extends JpaRepository<Member, Long> {
 
-    Optional<User> findUserByUsername(String username);
+    Optional<Member> findUserByUsername(String username);
 
 }
