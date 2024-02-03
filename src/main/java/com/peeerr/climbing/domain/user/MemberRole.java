2b757010e7c9cb2c3414e5ab6d796b87db21a00d
JunHyeongChoi <ajfeh22@gmail.com>
2024-02-03 16:51:09 +0900

    클래스명 변경 (#13)

--- src/main/java/com/peeerr/climbing/domain/user/MemberRole.java
+++ src/main/java/com/peeerr/climbing/domain/user/MemberRole.java
@@ -3,14 +3,14 @@ package com.peeerr.climbing.domain.user;
 import lombok.Getter;
 
 @Getter
-public enum UserRole {
+public enum MemberRole {
 
     ADMIN("ROLE_ADMIN"),
     USER("ROLE_USER");
 
     private String value;
 
-    UserRole(String value) {
+    MemberRole(String value) {
         this.value = value;
     }
 
