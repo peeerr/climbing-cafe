54c164e8b4771ffb687c0b819856864b2b9df786
JunHyeongChoi <ajfeh22@gmail.com>
2024-02-03 19:29:40 +0900

    `@Getter` 추가 (#13)

--- src/main/java/com/peeerr/climbing/config/auth/CustomUserDetails.java
+++ src/main/java/com/peeerr/climbing/config/auth/CustomUserDetails.java
@@ -1,6 +1,7 @@
 package com.peeerr.climbing.config.auth;
 
 import com.peeerr.climbing.domain.user.Member;
+import lombok.Getter;
 import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.userdetails.UserDetails;
@@ -9,6 +10,7 @@ import java.util.ArrayList;
 import java.util.Collection;
 import java.util.List;
 
+@Getter
 public class CustomUserDetails implements UserDetails {
 
     private final Member member;
