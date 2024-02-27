1082ec410ae9c78753164c352d0314fe063c06a1
JunHyeongChoi <ajfeh22@gmail.com>
2024-02-27 21:11:34 +0900

    [조회 성능 개선] N+1 문제 해결 및 카운트 쿼리 최적화 (#24)
    1.batch size 설정과 fetchJoin으로 N+1 문제 해결
    2. 페이징 시 카운트 쿼리는 join이 필요하지 않아 분리
    3. 마지막 페이지 조회시 카운트 쿼리 생략

--- src/main/java/com/peeerr/climbing/domain/post/CustomPostRepositoryImpl.java
+++ src/main/java/com/peeerr/climbing/domain/post/CustomPostRepositoryImpl.java
@@ -3,15 +3,23 @@ package com.peeerr.climbing.domain.post;
 import com.peeerr.climbing.dto.post.request.PostSearchCondition;
 import com.querydsl.core.types.dsl.BooleanExpression;
 import com.querydsl.core.util.StringUtils;
+import com.querydsl.jpa.impl.JPAQuery;
 import com.querydsl.jpa.impl.JPAQueryFactory;
 import jakarta.persistence.EntityManager;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.support.PageableExecutionUtils;
 
 import java.util.List;
 import java.util.Objects;
+import java.util.Optional;
 
+import static com.peeerr.climbing.domain.category.QCategory.category;
 import static com.peeerr.climbing.domain.post.QPost.post;
+import static com.peeerr.climbing.domain.user.QMember.member;
 
 public class CustomPostRepositoryImpl implements CustomPostRepository {
+
 
     private final JPAQueryFactory queryFactory;
 
@@ -20,13 +28,33 @@ public class CustomPostRepositoryImpl implements CustomPostRepository {
     }
 
     @Override
-    public List<Post> findPostsFilteredByCategoryIdAndSearchWord(Long categoryId, PostSearchCondition condition) {
+    public Page<Post> findPostsFilteredByCategoryIdAndSearchWord(Long categoryId, PostSearchCondition condition, Pageable pageable) {
         List<Post> posts = queryFactory
                 .selectFrom(post)
+                .join(post.category, category).fetchJoin()
+                .join(post.member, member).fetchJoin()
                 .where(categoryEq(categoryId), titleContains(condition.getTitle()), contentContains(condition.getContent()))
+                .orderBy(post.createDate.desc())
+                .offset(pageable.getOffset())
+                .limit(pageable.getPageSize())
                 .fetch();
 
-        return posts;
+        JPAQuery<Long> countQuery = queryFactory
+                .select(post.count())
+                .from(post)
+                .where(categoryEq(categoryId), titleContains(condition.getTitle()), contentContains(condition.getContent()));
+
+        return PageableExecutionUtils.getPage(posts, pageable, () -> countQuery.fetchOne());
+    }
+
+    @Override
+    public Optional<Post> findPostById(Long postId) {
+        return Optional.of(queryFactory
+                .selectFrom(post)
+                .join(post.category, category).fetchJoin()
+                .join(post.member, member).fetchJoin()
+                .where(post.id.eq(postId))
+                .fetchOne());
     }
 
     public BooleanExpression categoryEq(Long categoryId) {
