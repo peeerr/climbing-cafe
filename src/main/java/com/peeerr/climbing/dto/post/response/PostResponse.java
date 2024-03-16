9af6addfba1e4e1b2397aa34963f65660e5b2267
JunHyeongChoi <ajfeh22@gmail.com>
2024-03-16 20:36:08 +0900

    불필요한 정적 팩토리 메서드 제거 (#33)

--- src/main/java/com/peeerr/climbing/dto/post/response/PostResponse.java
+++ src/main/java/com/peeerr/climbing/dto/post/response/PostResponse.java
@@ -1,6 +1,5 @@
 package com.peeerr.climbing.dto.post.response;
 
-import com.peeerr.climbing.domain.post.Post;
 import com.querydsl.core.annotations.QueryProjection;
 import lombok.Getter;
 
@@ -15,14 +14,6 @@ public class PostResponse {
 
     private LocalDateTime createDate;
     private LocalDateTime modifyDate;
-
-    public static PostResponse from(Post post) {
-        return new PostResponse(post.getId(),
-                post.getTitle(),
-                post.getContent(),
-                post.getCreateDate(),
-                post.getModifyDate());
-    }
 
     @QueryProjection
     public PostResponse(Long postId, String categoryName, String writer, LocalDateTime createDate, LocalDateTime modifyDate) {
