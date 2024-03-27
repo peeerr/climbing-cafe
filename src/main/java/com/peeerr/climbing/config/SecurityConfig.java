a090b67dd5ae83b0c92f9391673f65acdc91b6b8
JunHyeongChoi <ajfeh22@gmail.com>
2024-03-27 15:17:58 +0900

    requestMachers & 필터 추가 및 예외처리 설정 (#37)

--- src/main/java/com/peeerr/climbing/config/SecurityConfig.java
+++ src/main/java/com/peeerr/climbing/config/SecurityConfig.java
@@ -1,49 +1,42 @@
 package com.peeerr.climbing.config;
 
-import com.peeerr.climbing.config.auth.CustomAuthFailureHandler;
-import com.peeerr.climbing.config.auth.CustomUserDetailsService;
+import com.peeerr.climbing.security.CustomAuthenticationEntryPoint;
+import com.peeerr.climbing.security.MemberAuthenticationFilter;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.authentication.AuthenticationProvider;
-import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 @RequiredArgsConstructor
 @Configuration
 public class SecurityConfig {
 
-    private final CustomUserDetailsService customUserDetailsService;
-    private final CustomAuthFailureHandler failureHandler;
+    private final MemberAuthenticationFilter memberAuthenticationFilter;
+    private final CustomAuthenticationEntryPoint entryPoint;
 
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
         http
                 .csrf(c -> c.disable())
-                .formLogin(form -> form
-                        .usernameParameter("email")
-                        .loginProcessingUrl("/api/members/login")
-                        .failureHandler(failureHandler))
+                .addFilterBefore(memberAuthenticationFilter, UsernamePasswordAuthenticationFilter.class)
                 .authorizeHttpRequests(auth -> auth
-                        .requestMatchers("/api/members/**").permitAll()
+                        .requestMatchers(HttpMethod.POST, "/api/members/**").permitAll()
+                        .requestMatchers(HttpMethod.GET,"/").permitAll()
+                        .requestMatchers(HttpMethod.GET, "/api/posts/**").permitAll()
+                        .requestMatchers(HttpMethod.GET, "/api/categories/**").permitAll()
+                        .requestMatchers(HttpMethod.GET, "/api/posts/{postId}/files/**").permitAll()
+                        .requestMatchers(HttpMethod.GET, "/api/posts/{postId}/likes/**").permitAll()
                         .requestMatchers("/api/**").authenticated()
-                        .anyRequest().permitAll());
+                        .anyRequest().permitAll())
+                .exceptionHandling(ex -> ex
+                        .authenticationEntryPoint(entryPoint));
 
         return http.build();
-    }
-
-    @Bean
-    public AuthenticationProvider authenticationProvider() {
-        DaoAuthenticationProvider provider = new DaoAuthenticationProvider();
-
-        provider.setUserDetailsService(customUserDetailsService);
-        provider.setPasswordEncoder(passwordEncoder());
-        provider.setHideUserNotFoundExceptions(false);
-
-        return provider;
     }
 
     @Bean
