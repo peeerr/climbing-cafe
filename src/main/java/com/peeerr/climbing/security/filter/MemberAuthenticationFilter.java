820807dd428c95b9b09ed0bded58ca3722753fc5
JunHyeongChoi <ajfeh22@gmail.com>
2024-05-10 00:48:41 +0900

    `@Component` 어노테이션 삭제
    불필요함

--- src/main/java/com/peeerr/climbing/security/filter/MemberAuthenticationFilter.java
+++ src/main/java/com/peeerr/climbing/security/filter/MemberAuthenticationFilter.java
@@ -10,9 +10,7 @@ import org.springframework.security.authentication.UsernamePasswordAuthenticatio
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.AuthenticationException;
 import org.springframework.security.web.authentication.AbstractAuthenticationProcessingFilter;
-import org.springframework.stereotype.Component;
 
-@Component
 public class MemberAuthenticationFilter extends AbstractAuthenticationProcessingFilter {
 
     private final ObjectMapper mapper;
