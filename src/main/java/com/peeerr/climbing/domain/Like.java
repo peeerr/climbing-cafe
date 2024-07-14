ce86cf1a7212d296d10525314f6944f0c4655421
JunHyeongChoi <ajfeh22@gmail.com>
2024-07-14 22:32:31 +0900

    refactor: 기본 생성자의 접근 제어자를 protected로 수정
    JPA 표준 스펙을 위해 생성한 것이기 때문에 외부에서 접근하지 못하도록 protected로 수정

--- src/main/java/com/peeerr/climbing/domain/Like.java
+++ src/main/java/com/peeerr/climbing/domain/Like.java
@@ -1,12 +1,13 @@
 package com.peeerr.climbing.domain;
 
 import jakarta.persistence.*;
+import lombok.AccessLevel;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
 @Getter
-@NoArgsConstructor
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
 @Table(name = "likes", uniqueConstraints = {
         @UniqueConstraint(
                 name = "like_uk",
