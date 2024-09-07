6789e917023e2379da719f2fca87583161bb4770
JunHyeongChoi <ajfeh22@gmail.com>
2024-09-07 19:05:34 +0900

    refactor: 검색 기능을 LIKE 쿼리에서 전문 검색 쿼리로 리팩토링

--- src/main/java/com/peeerr/climbing/repository/CustomPostRepositoryImpl.java
+++ src/main/java/com/peeerr/climbing/repository/CustomPostRepositoryImpl.java
@@ -1,28 +1,30 @@
 package com.peeerr.climbing.repository;
 
+import static com.peeerr.climbing.domain.QCategory.category;
+import static com.peeerr.climbing.domain.QMember.member;
+import static com.peeerr.climbing.domain.QPost.post;
+import static com.querydsl.core.types.Projections.constructor;
+
 import com.peeerr.climbing.domain.Post;
 import com.peeerr.climbing.dto.request.PostSearchCondition;
 import com.peeerr.climbing.dto.response.PopularPostResponse;
 import com.peeerr.climbing.dto.response.PostResponse;
 import com.querydsl.core.types.Projections;
 import com.querydsl.core.types.dsl.BooleanExpression;
+import com.querydsl.core.types.dsl.Expressions;
+import com.querydsl.core.types.dsl.StringPath;
 import com.querydsl.core.util.StringUtils;
 import com.querydsl.jpa.impl.JPAQuery;
 import com.querydsl.jpa.impl.JPAQueryFactory;
 import jakarta.persistence.EntityManager;
 import jakarta.persistence.PersistenceContext;
-import org.springframework.data.domain.Page;
-import org.springframework.data.domain.Pageable;
-import org.springframework.data.support.PageableExecutionUtils;
-
 import java.util.List;
 import java.util.Objects;
 import java.util.Optional;
-
-import static com.peeerr.climbing.domain.QCategory.category;
-import static com.peeerr.climbing.domain.QMember.member;
-import static com.peeerr.climbing.domain.QPost.post;
-import static com.querydsl.core.types.Projections.constructor;
+import java.util.stream.Stream;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.support.PageableExecutionUtils;
 
 public class CustomPostRepositoryImpl implements CustomPostRepository {
 
@@ -36,14 +38,14 @@ public class CustomPostRepositoryImpl implements CustomPostRepository {
     }
 
     @Override
-    public Page<PostResponse> findPostsFilteredByCategoryIdAndSearchWord(Long categoryId, PostSearchCondition condition, Pageable pageable) {
+    public Page<PostResponse> findPostsFilteredByCategoryIdAndSearchWord(Long categoryId, PostSearchCondition condition,
+                                                                         Pageable pageable) {
         List<Long> ids = queryFactory
                 .select(post.id)
                 .from(post)
                 .where(
                         categoryEq(categoryId),
-                        titleContains(condition.getTitle()),
-                        contentContains(condition.getContent())
+                        contains(post.title, post.content, condition)
                 )
                 .orderBy(post.createDate.desc())
                 .offset(pageable.getOffset())
@@ -69,7 +71,8 @@ public class CustomPostRepositoryImpl implements CustomPostRepository {
         JPAQuery<Long> countQuery = queryFactory
                 .select(post.count())
                 .from(post)
-                .where(categoryEq(categoryId), titleContains(condition.getTitle()), contentContains(condition.getContent()));
+                .where(categoryEq(categoryId), titleContains(condition.getTitle()),
+                        contentContains(condition.getContent()));
 
         return PageableExecutionUtils.getPage(posts, pageable, () -> countQuery.fetchOne());
     }
@@ -116,6 +119,26 @@ public class CustomPostRepositoryImpl implements CustomPostRepository {
 
     public BooleanExpression contentContains(String content) {
         return !StringUtils.isNullOrEmpty(content) ? post.content.contains(content) : null;
+    }
+
+    private BooleanExpression contains(StringPath title, StringPath content, PostSearchCondition condition) {
+        return Stream.of(
+                        condition.getTitle() != null
+                                ? Expressions.numberTemplate(Double.class,
+                                        "function('match_against', {0}, {1})",
+                                        title, condition.getTitle())
+                                .gt(0)
+                                : null,
+                        condition.getContent() != null
+                                ? Expressions.numberTemplate(Double.class,
+                                        "function('match_against', {0}, {1})",
+                                        content, condition.getContent())
+                                .gt(0)
+                                : null
+                )
+                .filter(Objects::nonNull)
+                .reduce(BooleanExpression::and)
+                .orElse(null);
     }
 
 }
