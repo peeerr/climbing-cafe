b7a1ac71ea8193b71588dc65777e93c9fd5fc5e2
JunHyeongChoi <ajfeh22@gmail.com>
2024-09-29 16:27:04 +0900

    feat: 이미지 파일 뿐만 아니라 다양한 타입 허용

--- src/main/java/com/peeerr/climbing/service/S3FileUploader.java
+++ src/main/java/com/peeerr/climbing/service/S3FileUploader.java
@@ -12,15 +12,13 @@ import com.peeerr.climbing.dto.FileStoreDto;
 import com.peeerr.climbing.exception.ClimbingException;
 import com.peeerr.climbing.exception.ErrorCode;
 import jakarta.annotation.PostConstruct;
-import lombok.RequiredArgsConstructor;
-import org.springframework.beans.factory.annotation.Value;
-import org.springframework.http.MediaType;
-import org.springframework.stereotype.Service;
-import org.springframework.web.multipart.MultipartFile;
-
 import java.io.IOException;
 import java.util.List;
 import java.util.UUID;
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Service;
+import org.springframework.web.multipart.MultipartFile;
 
 @RequiredArgsConstructor
 @Service
@@ -67,10 +65,11 @@ public class S3FileUploader {
         String filename = UUID.randomUUID() + "_" + file.getOriginalFilename();
 
         try {
-            String fileType = checkFileType(file);
+//            String fileType = checkFileType(file);
 
             ObjectMetadata objectMetadata = new ObjectMetadata();
-            objectMetadata.setContentType(fileType);
+            objectMetadata.setContentType(file.getContentType());
+            objectMetadata.setContentLength(file.getSize());
 
             amazonS3.putObject(new PutObjectRequest(bucket, filename, file.getInputStream(), objectMetadata)
                     .withCannedAcl(CannedAccessControlList.PublicRead));
@@ -86,16 +85,16 @@ public class S3FileUploader {
 //    }
 //
 
-    private String checkFileType(MultipartFile file) throws IOException {
-        String fileType = file.getContentType();
-
-        if (fileType != null && (fileType.equals(MediaType.IMAGE_JPEG_VALUE)
-                || fileType.equals(MediaType.IMAGE_PNG_VALUE)
-                || fileType.equals(MediaType.IMAGE_GIF_VALUE))) {
-            return fileType;
-        }
-
-        throw new ClimbingException(ErrorCode.INVALID_FILE_TYPE);
-    }
+//    private String checkFileType(MultipartFile file) throws IOException {
+//        String fileType = file.getContentType();
+//
+//        if (fileType != null && (fileType.equals(MediaType.IMAGE_JPEG_VALUE)
+//                || fileType.equals(MediaType.IMAGE_PNG_VALUE)
+//                || fileType.equals(MediaType.IMAGE_GIF_VALUE))) {
+//            return fileType;
+//        }
+//
+//        throw new ClimbingException(ErrorCode.INVALID_FILE_TYPE);
+//    }
 
 }
